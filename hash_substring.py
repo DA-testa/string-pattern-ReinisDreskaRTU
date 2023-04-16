37b85acbed83190ad9dd826baeda4fa909296193
221RDB277 <124622802+ReinisDreskaRTU@users.noreply.github.com>
2023-04-16 15:26:13 +0000

    -

--- hash_substring.py
+++ hash_substring.py
@@ -1,20 +1,6 @@
 # python3
 
 def read_input():
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    #return (input().rstrip(), input().rstrip())
-
     input_type = input()
 
     if "I" in input_type:
