6907e110c4d0c382011b9e3f15fe32992b418091
221RDB277 <124622802+ReinisDreskaRTU@users.noreply.github.com>
2023-04-16 15:23:50 +0000

    -

--- hash_substring.py
+++ hash_substring.py
@@ -13,20 +13,59 @@ def read_input():
     # return both lines in one return
     
     # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    #return (input().rstrip(), input().rstrip())
+
+    input_type = input()
+
+    if "I" in input_type:
+        pattern = input().rstrip()
+        text = input().rstrip()
+
+    elif "F" in input_type:
+        file_name = input()
+
+        if "test/" in file_name:
+            file_name.replace("test/", "tests/")
+
+        if "tests/" not in file_name:
+            file_name = "tests/" + file_name
+
+        with open(file_name, 'r') as file:
+            pattern = file.readline().rstrip()
+            text = file.readline().rstrip()
+
+    return(pattern, text)
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
-    # this function should find the occurances using Rabin Karp alghoritm 
+    n = len(text)
+    m = len(pattern)
+    p = 31
+    modulus = 10**9 + 9
 
-    # and return an iterable variable
-    return [0]
+    pattern_hash = 0
+    for i in range(m):
+        pattern_hash = (pattern_hash * p + ord(pattern[i])) % modulus
+
+    substr_hash = [0] * (n - m + 1)
+    substr_hash[0] = 0
+    for i in range(m):
+        substr_hash[0] = (substr_hash[0] * p + ord(text[i])) % modulus
+    for i in range(1, n - m + 1):
+        substr_hash[i] = ((substr_hash[i-1] - ord(text[i-1]) * pow(p, m-1, modulus)) * p + ord(text[i+m-1])) % modulus
+
+    occurrences = []
+    for i in range(n - m + 1):
+        if substr_hash[i] == pattern_hash:
+            if text[i:i+m] == pattern:
+                occurrences.append(i)
+    
+    return occurrences
 
 
 # this part launches the functions
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
-
